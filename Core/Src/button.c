b975aff856b54566fc98fa59d2aa0bde24ff4dfe
LPT0317 <75077760+LPT0317@users.noreply.github.com>
2022-11-05 13:29:54 +0700

    button

--- Core/Src/button.c
+++ Core/Src/button.c
@@ -21,7 +21,7 @@ uint16_t BUTTON[NO_BUTTON] = {RESET_Pin, INC_Pin, DEC_Pin};
  * Array with size = NUM_BUTTON
  * example button_flag[0]: check flag on button 0;
  */
-int button_buffer[NO_BUTTON] = {0};
+int button_flag[NO_BUTTON] = {0};
 /* Debounce var
  * Save the past state of button
  * debounce 3 time
@@ -32,7 +32,7 @@ int KeyReg2[NO_BUTTON] = {NORMAL_STATE};
 /* Save last state of button */
 int KeyReg3[NO_BUTTON] = {NORMAL_STATE};
 /* Set time for debounce */
-int timeOut[NO_BUTTON] = {200};
+int TimeOutForKeyPress[NO_BUTTON] = {300};
 
 /* Function -----*/
 /* Button reading and debounce */
@@ -44,14 +44,17 @@ void getKeyInput() {
     KeyReg0[i] = HAL_GPIO_ReadPin(GPIOA, BUTTON[i]);
     if((KeyReg1[i] == KeyReg0[i]) && (KeyReg1[i] == KeyReg2[i])) {
       if(KeyReg3[i] != KeyReg2[i]) {
-	    KeyReg3[i] = KeyReg2[i];
-	    button_buffer[i] = KeyReg0[i];
+    	KeyReg3[i] = KeyReg2[i];
+    	if(KeyReg3[i] == PRESSED_STATE) {
+    	  TimeOutForKeyPress[i] = 300;
+    	  button_flag[i] = 1;
+    	}
       }
       else {
-	    timeOut[i]--;
-	    if(timeOut[i] == 0) {
-    	  KeyReg3[i] = NORMAL_STATE;
-	    }
+        TimeOutForKeyPress[i]--;
+        if(TimeOutForKeyPress[i] == 0) {
+          KeyReg3[i] = NORMAL_STATE;
+        }
       }
     }
   }
@@ -62,7 +65,10 @@ void getKeyInput() {
  */
 int isButtonPressed(int index) {
   if(checkIndex(index, NO_BUTTON)) {
-    return (button_buffer[index] == PRESSED_STATE);
+    if(button_flag[index] == 1) {
+      button_flag[index] = 0;
+      return 1;
+    }
   }
   return 0;
 }
